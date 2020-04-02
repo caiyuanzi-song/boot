3b22c6800815886d6cc27389f1efaa25952b0095
913062247@qq.com <123456789>
2020-04-02 19:54:19 +0800

    boot web

---
+++ boot-web/src/main/java/com/cyz/boot/WebApplication.java
@@ -0,0 +1,16 @@
+package com.cyz.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author:cyz
+ * @Date:2020/4/2 19:48
+ * @Description:
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
